4e2c0218866aeefca23495a304f738b2b25ddc39
Julio Soto Sánchez <79101920+404BadRequest@users.noreply.github.com>
2024-03-14 12:28:00 -0300

    Feat: Cambios en dashboard y ordenamiento de datos para presentación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,13 @@ import { AppLayoutComponent } from "./layout/app.layout.component";
             {
                 path: '', component: AppLayoutComponent,
                 children: [
-                    { path: '', loadChildren: () => import('./reserva/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+                    { path: 'dashboard', loadChildren: () => import('./reserva/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
                     { path: 'uikit', loadChildren: () => import('./reserva/components/uikit/uikit.module').then(m => m.UIkitModule) },
                     { path: 'utilities', loadChildren: () => import('./reserva/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
                     { path: 'documentation', loadChildren: () => import('./reserva/components/documentation/documentation.module').then(m => m.DocumentationModule) },
                     { path: 'blocks', loadChildren: () => import('./reserva/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-                    { path: 'pages', loadChildren: () => import('./reserva/components/pages/pages.module').then(m => m.PagesModule) }
+                    { path: 'pages', loadChildren: () => import('./reserva/components/pages/pages.module').then(m => m.PagesModule) },
+                    
                 ]
             },
             { path: 'auth', loadChildren: () => import('./reserva/components/auth/auth.module').then(m => m.AuthModule) },
