14f941e67cb43253dc9b8bf5f8d059300505ac53
Julio Soto Sánchez <ju.soto.sanchez@gmail.com>
2024-05-08 12:33:07 -0400

    Feat: Login con Ad azure SAML

--- src/app/reserva/components/auth/login/login.component.ts
+++ src/app/reserva/components/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalService } from '@azure/msal-angular';
+import { AuthenticationResult, InteractionStatus, InteractionType, PopupRequest, RedirectRequest } from '@azure/msal-browser';
+import { Subject, filter, takeUntil } from 'rxjs';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
@@ -13,11 +16,71 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
         }
     `]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy{
 
     valCheck: string[] = ['remember'];
-
     password!: string;
+    isIframe = false;
+    loginDisplay = false;
+    private readonly _destroying$ = new Subject<void>();
 
-    constructor(public layoutService: LayoutService) { }
+
+    constructor(
+        @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
+        public layoutService: LayoutService,
+        private authService: MsalService,
+        private msalBroadcastService: MsalBroadcastService
+    ) { }
+
+    async ngOnInit(): Promise<any> {
+        await this.authService.instance.initialize();
+        this.authService.instance.handleRedirectPromise().then((res) => {
+          if (res != null && res.account != null) {
+            this.authService.instance.setActiveAccount(res.account);
+          }
+        });
+      }
+      setLoginDisplay() {
+        this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+      }
+    
+      login() {
+        if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
+          if (this.msalGuardConfig.authRequest) {
+            this.authService.loginPopup({ ...this.msalGuardConfig.authRequest } as PopupRequest)
+              .subscribe((response: AuthenticationResult) => {
+                this.authService.instance.setActiveAccount(response.account);
+              });
+          } else {
+            this.authService.loginPopup()
+              .subscribe((response: AuthenticationResult) => {
+                this.authService.instance.setActiveAccount(response.account);
+              });
+          }
+        } else {
+          if (this.msalGuardConfig.authRequest) {
+            this.authService.loginRedirect({ ...this.msalGuardConfig.authRequest } as RedirectRequest);
+          } else {
+            this.authService.loginRedirect();
+          }
+        }
+      }
+    
+      logout() {
+        if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
+          this.authService.logoutPopup({
+            postLogoutRedirectUri: "/",
+            mainWindowRedirectUri: "/"
+          });
+        } else {
+          this.authService.logoutRedirect({
+            postLogoutRedirectUri: "/",
+          });
+        }
+      }
+    
+      ngOnDestroy(): void {
+        this._destroying$.next(undefined);
+        this._destroying$.complete();
+      }
 }
--- src/app/reserva/components/auth/login/login.module.ts
+++ src/app/reserva/components/auth/login/login.module.ts
@@ -7,7 +7,77 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {
+    IPublicClientApplication,
+    PublicClientApplication,
+    BrowserCacheLocation,
+    LogLevel,
+    InteractionType,
+  } from '@azure/msal-browser';
+  import {
+    MSAL_INSTANCE,
+    MSAL_INTERCEPTOR_CONFIG,
+    MsalInterceptorConfiguration,
+    MSAL_GUARD_CONFIG,
+    MsalGuardConfiguration,
+    MsalBroadcastService,
+    MsalService,
+    MsalGuard,
+    MsalRedirectComponent,
+    MsalModule,
+    MsalInterceptor,
+  } from '@azure/msal-angular';
 
+  
+  
+  const GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0/me';
+  const isIE =
+  window.navigator.userAgent.indexOf('MSIE ') > -1 ||
+  window.navigator.userAgent.indexOf('Trident/') > -1;
+
+  export function loggerCallback(logLevel: LogLevel, message: string) {
+    console.log(message);
+  }
+
+  export function MSALInstanceFactory(): IPublicClientApplication {
+    return new PublicClientApplication({
+      auth: {
+        clientId: 'f9432d3e-1e87-461f-bba7-0fa1db5a4730',
+        authority: 'https://login.microsoftonline.com/e5835003-ab1f-4e4b-8020-c5886fbc8369',
+        redirectUri: 'http://localhost:4200',
+      },
+      cache: {
+        cacheLocation: BrowserCacheLocation.LocalStorage,
+        storeAuthStateInCookie: isIE, // set to true for IE 11
+      },
+      system: {
+        loggerOptions: {
+          loggerCallback,
+          logLevel: LogLevel.Info,
+          piiLoggingEnabled: false,
+        },
+      },
+    });
+  }
+  export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+    const protectedResourceMap = new Map<string, Array<string>>();
+    protectedResourceMap.set(GRAPH_ENDPOINT, ['user.read']);
+  
+    return {
+      interactionType: InteractionType.Redirect,
+      protectedResourceMap,
+    };
+  }
+  
+  export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+    return {
+      interactionType: InteractionType.Redirect,
+      authRequest: {
+        scopes: ['user.read'],
+      },
+    };
+  }
 @NgModule({
     imports: [
         CommonModule,
@@ -16,8 +86,33 @@ import { InputTextModule } from 'primeng/inputtext';
         CheckboxModule,
         InputTextModule,
         FormsModule,
-        PasswordModule
+        PasswordModule,
+        HttpClientModule,
+        MsalModule
     ],
-    declarations: [LoginComponent]
+    declarations: [LoginComponent],
+    providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: MsalInterceptor,
+          multi: true,
+        },
+        {
+          provide: MSAL_INSTANCE,
+          useFactory: MSALInstanceFactory,
+        },
+        {
+          provide: MSAL_GUARD_CONFIG,
+          useFactory: MSALGuardConfigFactory,
+        },
+        {
+          provide: MSAL_INTERCEPTOR_CONFIG,
+          useFactory: MSALInterceptorConfigFactory,
+        },
+        MsalService,
+        MsalGuard,
+        MsalBroadcastService,
+      ],
+      bootstrap: [LoginComponent, MsalRedirectComponent]
 })
 export class LoginModule { }
